e491bce093983ef0fd28d0b91fa25484bf0fdf6e
AntonenkoTymofii <antonenkotimofii@gmail.com>
2024-09-22 17:01:24 +0300

    create project

---
+++ src/main/java/org/example/backendlab1/BackEndLab1Application.java
@@ -0,0 +1,13 @@
+package org.example.backendlab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndLab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndLab1Application.class, args);
+    }
+
+}
